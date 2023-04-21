bce1c01cd2eaa5073cde7927356d5c28485c3d10
Chelsea Johnson <chelseajohnson@macbook-air.lan>
2023-04-21 10:33:16 -0600

    Add roll dice button

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,27 @@ import Dice from './components/Dice'
 import './App.css';
 
 function App() {
+
+  const [dice, setDice] = React.useState(newDice())
+
+  const dieElements = dice.map(die => <Dice value={die} />)
+
+  function newDice() {
+    return [...Array(10)].map(() => Math.ceil(Math.random() * 6))
+  }
+
+  function rollDice() {
+    setDice(newDice())
+  }
+
   return (
     <main className="box">
       <h1>Tenzies!</h1>
       <div className="dice">
-          <Dice value={1} />
-          <Dice value={1} />
-          <Dice value={1} />
-          <Dice value={1} />
-          <Dice value={1} />
-          <Dice value={1} />
-          <Dice value={1} />
-          <Dice value={1} />
-          <Dice value={1} />
-          <Dice value={1} />
+          {dieElements}
       </div>
+
+      <button className="roll-button" onClick={rollDice}>Roll</button>
       
     </main>
   )
