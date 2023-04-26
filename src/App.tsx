543a50c06cd64e840cb4caa218b018709b3a3a3b
Chelsea Johnson <chelseajohnson@macbook-air.lan>
2023-04-26 09:36:06 -0600

    Adds hold dice and game winning condition
    confetti too

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,68 @@
+import Confetti from 'react-confetti';
 import React from 'react';
+import {nanoid} from 'nanoid';
 import Dice from './components/Dice'
 import './App.css';
+
+interface Die {
+  id: string,
+  isHeld: boolean,
+  number: number
+}
 
 function App() {
 
   const [dice, setDice] = React.useState(newDice())
 
-  const dieElements = dice.map(die => <Dice value={die} />)
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(() =>{
+    const firstNumber = dice[0].number
+    const isGameWon = dice.every(die => die.isHeld && die.number == firstNumber)
+    if (isGameWon) {
+      console.log('setting tenzies')
+      setTenzies(true)
+    }
+
+  }, [dice])
+
+  const dieElements = dice.map(die => 
+    <Dice key={die.id} isHeld={die.isHeld} value={die.number} holdDice={() => holdDice(die.id)}/>
+  )
+
+  function newDie(): Die {
+    return {
+      id: nanoid(),
+      isHeld: false,
+      number: Math.ceil(Math.random() * 6)
+    }
+  }
 
   function newDice() {
-    return [...Array(10)].map(() => Math.ceil(Math.random() * 6))
+    return [...Array(10)].map(() => newDie())
   }
 
   function rollDice() {
-    setDice(newDice())
+    setDice(prevDice => prevDice.map(die => die.isHeld ? die : newDie()))
   }
+
+  function holdDice(id: string) {
+    setDice(prevDice => {
+      return prevDice.map(die => die.id === id ? {...die, isHeld: !die.isHeld} : die)
+    })
+  }
+
 
   return (
     <main className="box">
       <h1>Tenzies!</h1>
+      <p>Roll until all dice are the same! Click a die to hold its value.</p>
       <div className="dice">
           {dieElements}
       </div>
 
-      <button className="roll-button" onClick={rollDice}>Roll</button>
-      
+      <button className="roll-button" onClick={rollDice}>{tenzies ? 'New Game' : 'Roll'}</button>
+      {tenzies && <Confetti />}
     </main>
   )
 }
--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,6 +1,9 @@
-export default function Dice(props: {value: number}) {
+export default function Dice(props: {value: number, isHeld: boolean, holdDice: () => void}) {
+    const buttonStyle = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
     return (
-        <div className="die">
+        <div className="die" style={buttonStyle} onClick={props.holdDice}>
             <h2>{props.value}</h2>
         </div>
     )
